10d3ddb8edc4641896fa14441dc3eb8bb8ec0e0b
李胜霖 <741509097@qq.com>
2016-08-24 11:55:20 +0800

    初始化

---
+++ OCToCollectionView/OCToCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCToCollectionView
+//
+//  Created by 李天空 on 16/8/24.
+//  Copyright © 2016年 李天空. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
